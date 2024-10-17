1d9b43493577e540ad926c964f7df76848d7c318
Vinícius Campitelli <1877191+vcampitelli@users.noreply.github.com>
2024-10-17 20:03:20 -0300

    Retirando o ponto final

--- demo/frontend/src/Home.js
+++ demo/frontend/src/Home.js
@@ -6,7 +6,7 @@ function Home() {
             <div className="container-fluid py-5">
                 <h1 className="display-5 fw-bold">Bem-vindo!</h1>
                 <p className="col-md-8 fs-4">
-                    Essa é uma aplicação para demonstrar um <em>frontend</em> rodando na Nuvem.
+                    Essa é uma aplicação para demonstrar um <em>frontend</em> rodando na Nuvem
                 </p>
                 <Link to="/users" className="btn btn-primary btn-lg">
                     Listar usuários
